772d0ba039c4d6c9838d8577f092c205d99bc7bb
Elnur Shirmamammadov <shirmammadovelnur@gmail.com>
2024-05-25 18:27:38 +0400

    set scheduled date

--- src/main/java/az/travellab/ms_travel_application/scheduler/NotificationScheduler.java
+++ src/main/java/az/travellab/ms_travel_application/scheduler/NotificationScheduler.java
@@ -14,34 +14,34 @@ public class NotificationScheduler {
     private final ClientNotificationService clientNotificationService;
 
     @Async
-    @Scheduled(fixedRateString = "PT15M")
-//    @Scheduled(cron = "0 0 19 * * *")
+//    @Scheduled(fixedRateString = "PT15M")
+    @Scheduled(cron = "0 0 15 * * *")
     @SchedulerLock(name = "sendTripReminderOneDayBefore", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
     public void sendTripReminderOneDayBefore() {
         clientNotificationService.sendTripReminderOneDayBefore();
     }
 
 //    @Async
-//    @Scheduled(fixedRateString = "PT15M")
+//    @Scheduled(cron = "0 0 8 * * *")
 //    @SchedulerLock(name = "sendTripReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
 //    public void sendTripReminder() {
 //        clientNotificationService.sendTripReminder();
 //    }
 //
 //    @Async
-//    @Scheduled(fixedRateString = "PT15M")
+//    @Scheduled(cron = "0 0 8 * * *")
 //    @SchedulerLock(name = "returnReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
 //    public void returnReminder() {
 //        clientNotificationService.returnReminder();
 //    }
 //    @Async
-//    @Scheduled(fixedRateString = "PT15M")
+//    @Scheduled(cron = "0 0 8 * * *")
 //    @SchedulerLock(name = "initialPaymentDateReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
 //    public void initialPaymentDateReminder() {
 //        clientNotificationService.initialPaymentDateReminder();
 //    }
 //    @Async
-//    @Scheduled(fixedRateString = "PT15M")
+//    @Scheduled(cron = "0 0 8 * * *")
 //    @SchedulerLock(name = "paymentDateReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
 //    public void paymentDateReminder() {
 //        clientNotificationService.paymentDateReminder();
