1dc798edc3bebaa2d4846fa3ef66f883d772f3e1
Elnur Shirmamammadov <shirmammadovelnur@gmail.com>
2024-06-08 16:54:22 +0400

    scheduller enable

--- src/main/java/az/travellab/ms_travel_application/scheduler/NotificationScheduler.java
+++ src/main/java/az/travellab/ms_travel_application/scheduler/NotificationScheduler.java
@@ -13,43 +13,43 @@ public class NotificationScheduler {
 
     private final ClientNotificationService clientNotificationService;
 
-//    @Async
-//    @Scheduled(cron = "0 0 8 * * *")
-//    @SchedulerLock(name = "sendTripReminderOneDayBefore", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
-//    public void sendTripReminderOneDayBefore() {
-//        clientNotificationService.sendTripReminderOneDayBefore();
-//    }
-//
-//    @Async
-//    @Scheduled(cron = "0 0 8 * * *")
-//    @SchedulerLock(name = "sendTripReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
-//    public void sendTripReminder() {
-//        clientNotificationService.sendTripReminder();
-//    }
-//
-//    @Async
-//    @Scheduled(cron = "0 0 8 * * *")
-//    @SchedulerLock(name = "returnReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
-//    public void returnReminder() {
-//        clientNotificationService.returnReminder();
-//    }
-//    @Async
-//    @Scheduled(cron = "0 0 8 * * *")
-//    @SchedulerLock(name = "initialPaymentDateReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
-//    public void initialPaymentDateReminder() {
-//        clientNotificationService.initialPaymentDateReminder();
-//    }
-//    @Async
-//    @Scheduled(cron = "0 0 8 * * *")
-//    @SchedulerLock(name = "paymentDateReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
-//    public void paymentDateReminder() {
-//        clientNotificationService.paymentDateReminder();
-//    }
-//
-//    @Async
-//    @Scheduled(cron = "0 0 8 * * *")
-//    @SchedulerLock(name = "sendBirthdayGreetings", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
-//    public void sendBirthdayGreetings() {
-//        clientNotificationService.sendBirthdayGreetings();
-//    }
+    @Async
+    @Scheduled(cron = "0 0 8 * * *")
+    @SchedulerLock(name = "sendTripReminderOneDayBefore", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
+    public void sendTripReminderOneDayBefore() {
+        clientNotificationService.sendTripReminderOneDayBefore();
+    }
+
+    @Async
+    @Scheduled(cron = "0 0 8 * * *")
+    @SchedulerLock(name = "sendTripReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
+    public void sendTripReminder() {
+        clientNotificationService.sendTripReminder();
+    }
+
+    @Async
+    @Scheduled(cron = "0 0 8 * * *")
+    @SchedulerLock(name = "returnReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
+    public void returnReminder() {
+        clientNotificationService.returnReminder();
+    }
+    @Async
+    @Scheduled(cron = "0 0 8 * * *")
+    @SchedulerLock(name = "initialPaymentDateReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
+    public void initialPaymentDateReminder() {
+        clientNotificationService.initialPaymentDateReminder();
+    }
+    @Async
+    @Scheduled(cron = "0 0 8 * * *")
+    @SchedulerLock(name = "paymentDateReminder", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
+    public void paymentDateReminder() {
+        clientNotificationService.paymentDateReminder();
+    }
+
+    @Async
+    @Scheduled(cron = "0 0 8 * * *")
+    @SchedulerLock(name = "sendBirthdayGreetings", lockAtLeastFor = "PT1M", lockAtMostFor = "PT5M")
+    public void sendBirthdayGreetings() {
+        clientNotificationService.sendBirthdayGreetings();
+    }
 }
